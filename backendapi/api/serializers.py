effcb1d3c25e19f6981c72a93c8bbce3b204946c
dominhquan2003 <doquan23032003@gmail.com>
2025-02-10 22:18:11 +0700

    intergrate model and face detect in POST api.

--- backendapi/api/serializers.py
+++ backendapi/api/serializers.py
@@ -12,3 +12,9 @@ class PerformerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Performer
         fields = ['id', 'name', 'status', 'original_image', 'detected_image', 'heatmap_1', 'heatmap_2', 'heatmap_3']
+    def create(self, validated_data):
+        # Nếu không có status, gán giá trị mặc định là 'True'
+        if 'status' not in validated_data or validated_data['status'] is None:
+            validated_data['status'] = 'True'
+        
+        return super().create(validated_data)
--- backendapi/api/views.py
+++ backendapi/api/views.py
@@ -3,11 +3,39 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Performer
 from .serializers import PerformerSerializer
-
+from .utils import detect_faces, get_distance_vector
+import numpy as np
+import cv2
+import os
 class PerfromerListCreate(generics.ListCreateAPIView):
     queryset = Performer.objects.all()
     serializer_class = PerformerSerializer
+    
+    def create(self, request, *args, **kwargs):
+        original_image = request.FILES.get('original_image')
+        name=request.data.get('name')
+        if not original_image:
+            return Response({'error': 'No image provided'}, status=status.HTTP_400_BAD_REQUEST)
 
+        np_image = np.frombuffer(original_image.read(), np.uint8)
+        img = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+        detected_faces = detect_faces(img)
+        save_folder = os.path.join('images', name)  # Tạo đường dẫn thư mục
+        if not detected_faces:
+            return Response({'error': 'No face detected'}, status=status.HTTP_400_BAD_REQUEST)
+
+        for i, face_img in enumerate(detected_faces):
+            distances = get_distance_vector(face_img)
+            performer = Performer.objects.create(
+                name=name,
+                original_image=original_image,
+            )
+            face_crop_name = f"face_crop_{performer.pk}.jpg"  # Tên file lưu
+            face_crop_path = os.path.join(save_folder, face_crop_name)  # Đường dẫn lưu
+            cv2.imwrite(face_crop_path, face_img)  # Lưu hình ảnh khuôn mặt
+        serializer = self.get_serializer(performer)
+        print(distances, serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 class PerformerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Performer.objects.all()
     serializer_class = PerformerSerializer
